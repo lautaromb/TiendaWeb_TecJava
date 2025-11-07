57423e6d6eb2bf238344780bec2b90a3eabd2638
Lautaro <76851233+lautaromb@users.noreply.github.com>
2025-11-07 16:12:11 -0300

    Initial commit

---
+++ tiendaweb/src/main/java/com/tecnologiaJava/tiendaweb/TiendawebApplication.java
@@ -0,0 +1,13 @@
+package com.tecnologiaJava.tiendaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendawebApplication.class, args);
+	}
+
+}
---
+++ tiendaweb/src/test/java/com/tecnologiaJava/tiendaweb/TiendawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnologiaJava.tiendaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
